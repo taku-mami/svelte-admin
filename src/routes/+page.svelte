f2c9d7851a52a4e593c73f731204dca01e66e99d
윤성탁 <arch931229@gmail.com>
2024-11-02 22:10:57 +0900

    날짜 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import { goto } from '$app/navigation';
 
+    let today = new Date().toISOString().split('T')[0];
     let userId = '';
     let password = '';
 
@@ -136,6 +137,7 @@
   <div class="container mx-auto">
     <h1 class="text-3xl font-bold mb-5">오크베리 {branch}</h1>
     <h2 class="text-2xl font-bold mb-5">원료 재고 관리 시트</h2>
+    <h3 class="text-xl font-bold mb-5">날짜: {today}</h3>
     <table class="min-w-full bg-white border border-gray-300 rounded-lg">
       <thead>
         <tr class="bg-gray-100 border-b border-gray-300">
